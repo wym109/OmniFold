3240038d768ad625b1601baec121d9b6a01fa55d
Wing Yan Ma <wing.yan.ma@desy.de>
2024-07-17 10:47:15 +0800

    adding files

--- omnifold.py
+++ omnifold.py
@@ -1,114 +1,140 @@
+import argparse
+import gc
+import os
+import sys
+import time
+
+import energyflow as ef
 import numpy as np
-import tensorflow as tf
-import tensorflow.keras.backend as K
-from sklearn.model_selection import train_test_split
 
-def reweight(events,model,batch_size=10000):
-    f = model.predict(events, batch_size=batch_size)
-    weights = f / (1. - f)
-    return np.squeeze(np.nan_to_num(weights))
+# DCTR, reweights positive distribution to negative distribution
+# X: features
+# Y: categorical labels
+# model: model with fit/predict
+# fitargs: model fit arguments
+def reweight(X, Y, w, model, filepath, fitargs, val_data=None):
 
-# Binary crossentropy for classifying two samples with weights
-# Weights are "hidden" by zipping in y_true (the labels)
+    # permute the data, fit the model, and get preditions
+    #perm = np.random.permutation(len(X))
+    #model.fit(X[perm], Y[perm], sample_weight=w[perm], **fitargs)
+    val_dict = {'validation_data': val_data} if val_data is not None else {}
+    model.fit(X, Y, sample_weight=w, **fitargs, **val_dict)
+    model.save_weights(filepath)
+    preds = model.predict(X, batch_size=10*fitargs.get('batch_size', 500))[:,1]
+    
+    # concatenate validation predictions into training predictions
+    if val_data is not None:
+        preds_val = model.predict(val_data[0], batch_size=10*fitargs.get('batch_size', 500))[:,1]
+        preds = np.concatenate((preds, preds_val))
+        w = np.concatenate((w, val_data[2]))
 
-def weighted_binary_crossentropy(y_true, y_pred):
-    weights = tf.gather(y_true, [1], axis=1) # event weights
-    y_true = tf.gather(y_true, [0], axis=1) # actual y_true for loss
+    w *= np.clip(preds/(1 - preds + 10**-50), fitargs.get('weight_clip_min', 0.), fitargs.get('weight_clip_max', np.inf))
+    return w
 
-    # Clip the prediction value to prevent NaN's and Inf's
-    epsilon = K.epsilon()
-    y_pred = K.clip(y_pred, epsilon, 1. - epsilon)
+# OmniFold
+# X_gen/Y_gen: particle level features/labels
+# X_det/Y_det: detector level features/labels, note these should be ordered as (data, sim)
+# wdata/winit: initial weights of the data/simulation
+# model: model with fit/predict
+# fitargs: model fit arguments
+# it: number of iterations
+# trw_ind: which previous weights to use in second step, 0 means use initial, -2 means use previous
+def omnifold(X_gen_i, Y_gen_i, X_det_i, Y_det_i, wdata, winit, det_model, mc_model, fitargs, 
+             val=0.2, it=10, weights_filename=None, trw_ind=0, delete_global_arrays=False):
 
-    t_loss = -weights * ((y_true) * K.log(y_pred) +
-                         (1 - y_true) * K.log(1 - y_pred))
+    # get arrays (possibly globally)
+    X_gen_arr = globals()[X_gen_i] if isinstance(X_gen_i, str) else X_gen_i
+    Y_gen_arr = globals()[Y_gen_i] if isinstance(Y_gen_i, str) else Y_gen_i
+    X_det_arr = globals()[X_det_i] if isinstance(X_det_i, str) else X_det_i
+    Y_det_arr = globals()[Y_det_i] if isinstance(Y_det_i, str) else Y_det_i
+    
+    # initialize the truth weights to the prior
+    ws = [winit]
+    
+    # get permutation for det
+    perm_det = np.random.permutation(len(winit) + len(wdata))
+    invperm_det = np.argsort(perm_det)
+    nval_det = int(val*len(perm_det))
+    X_det_train, X_det_val = X_det_arr[perm_det[:-nval_det]], X_det_arr[perm_det[-nval_det:]]
+    Y_det_train, Y_det_val = Y_det_arr[perm_det[:-nval_det]], Y_det_arr[perm_det[-nval_det:]]
 
-    return K.mean(t_loss)
+    # remove X_det, Y_det
+    if delete_global_arrays:
+        del X_det_arr, Y_det_arr
+        if isinstance(X_det_i, str):
+            del globals()[X_det_i]
+        if isinstance(Y_det_i, str):
+            del globals()[Y_det_i]
+    
+    # get an initial permutation for gen and duplicate (offset) it
+    nval = int(val*len(winit))
+    baseperm0 = np.random.permutation(len(winit))
+    baseperm1 = baseperm0 + len(winit)
+    
+    # training examples are at beginning, val at end
+    # concatenate into single train and val perms (shuffle each)
+    trainperm = np.concatenate((baseperm0[:-nval], baseperm1[:-nval]))
+    valperm = np.concatenate((baseperm0[-nval:], baseperm1[-nval:]))
+    np.random.shuffle(trainperm)
+    np.random.shuffle(valperm)
+    
+    # get final permutation for gen (ensured that the same events end up in val)
+    perm_gen = np.concatenate((trainperm, valperm))
+    invperm_gen = np.argsort(perm_gen)
+    nval_gen = int(val*len(perm_gen))
+    X_gen_train, X_gen_val = X_gen_arr[perm_gen[:-nval_gen]], X_gen_arr[perm_gen[-nval_gen:]]
+    Y_gen_train, Y_gen_val = Y_gen_arr[perm_gen[:-nval_gen]], Y_gen_arr[perm_gen[-nval_gen:]]
 
-def omnifold(theta0,theta_unknown_S,iterations,model,verbose=0):
+    # remove X_gen, Y_gen
+    if delete_global_arrays:
+        del X_gen_arr, Y_gen_arr
+        if isinstance(X_gen_i, str):
+            del globals()[X_gen_i]
+        if isinstance(Y_gen_i, str):
+            del globals()[Y_gen_i]
 
-    weights = np.empty(shape=(iterations, 2, len(theta0)))
-    # shape = (iteration, step, event)
+    # store model filepaths
+    model_det_fp, model_mc_fp = det_model[1].get('filepath', None), mc_model[1].get('filepath', None)
+    
+    # iterate the procedure
+    for i in range(it):
 
-    theta0_G = theta0[:,0]
-    theta0_S = theta0[:,1]
+        # det filepaths properly
+        if model_det_fp is not None:
+            model_det_fp_i = model_det_fp.format(i)
+            det_model[1]['filepath'] = model_det_fp_i + '_Epoch-{epoch}'
+        if model_mc_fp is not None:
+            model_mc_fp_i = model_mc_fp.format(i)
+            mc_model[1]['filepath'] = model_mc_fp_i + '_Epoch-{epoch}'
 
-    labels0 = np.zeros(len(theta0))
-    labels_unknown = np.ones(len(theta_unknown_S))
-    labels_unknown_step2 = np.ones(len(theta0_G))
+        # define models
+        model_det = det_model[0](**det_model[1])
+        model_mc = mc_model[0](**mc_model[1])
 
-    xvals_1 = np.concatenate((theta0_S, theta_unknown_S))
-    yvals_1 = np.concatenate((labels0, labels_unknown))
+        # load weights if not model 0
+        if i > 0:
+            model_det.load_weights(model_det_fp.format(i-1))
+            model_mc.load_weights(model_mc_fp.format(i-1))
+        
+        # step 1: reweight sim to look like data
+        w = np.concatenate((wdata, ws[-1]))
+        w_train, w_val = w[perm_det[:-nval_det]], w[perm_det[-nval_det:]]
+        rw = reweight(X_det_train, Y_det_train, w_train, model_det, model_det_fp_i,
+                      fitargs, val_data=(X_det_val, Y_det_val, w_val))[invperm_det]
+        ws.append(rw[len(wdata):])
 
-    xvals_2 = np.concatenate((theta0_G, theta0_G))
-    yvals_2 = np.concatenate((labels0, labels_unknown_step2))
+        # step 2: reweight the prior to the learned weighting
+        w = np.concatenate((ws[-1], ws[trw_ind]))
+        w_train, w_val = w[perm_gen[:-nval_gen]], w[perm_gen[-nval_gen:]]
+        rw = reweight(X_gen_train, Y_gen_train, w_train, model_mc, model_mc_fp_i,
+                      fitargs, val_data=(X_gen_val, Y_gen_val, w_val))[invperm_gen]
+        ws.append(rw[len(ws[-1]):])
+        
+        # save the weights if specified
+        if weights_filename is not None:
+            np.save(weights_filename, ws)
+        
+    return ws
 
-    # initial iterative weights are ones
-    weights_pull = np.ones(len(theta0_S))
-    weights_push = np.ones(len(theta0_S))
-
-    for i in range(iterations):
-
-        if (verbose>0):
-            print("\nITERATION: {}\n".format(i + 1))
-            pass
-
-        # STEP 1: classify Sim. (which is reweighted by weights_push) to Data
-        # weights reweighted Sim. --> Data
-
-        if (verbose>0):
-            print("STEP 1\n")
-            pass
-
-        weights_1 = np.concatenate((weights_push, np.ones(len(theta_unknown_S))))
-
-        X_train_1, X_test_1, Y_train_1, Y_test_1, w_train_1, w_test_1 = train_test_split(xvals_1, yvals_1, weights_1)
-
-        # zip ("hide") the weights with the labels
-        Y_train_1 = np.stack((Y_train_1, w_train_1), axis=1)
-        Y_test_1 = np.stack((Y_test_1, w_test_1), axis=1)   
-
-        model.compile(loss=weighted_binary_crossentropy,
-                      optimizer='Adam',
-                      metrics=['accuracy'])
-
-        model.fit(X_train_1,
-                  Y_train_1,
-                  epochs=20,
-                  batch_size=10000,
-                  validation_data=(X_test_1, Y_test_1),
-                  verbose=verbose)
-
-        weights_pull = weights_push * reweight(theta0_S,model)
-        weights[i, :1, :] = weights_pull
-
-        # STEP 2: classify Gen. to reweighted Gen. (which is reweighted by weights_pull)
-        # weights Gen. --> reweighted Gen.
-
-        if (verbose>0):
-            print("\nSTEP 2\n")
-            pass
-
-        weights_2 = np.concatenate((np.ones(len(theta0_G)), weights_pull))
-        # ones for Gen. (not MC weights), actual weights for (reweighted) Gen.
-
-        X_train_2, X_test_2, Y_train_2, Y_test_2, w_train_2, w_test_2 = train_test_split(xvals_2, yvals_2, weights_2)
-
-        # zip ("hide") the weights with the labels
-        Y_train_2 = np.stack((Y_train_2, w_train_2), axis=1)
-        Y_test_2 = np.stack((Y_test_2, w_test_2), axis=1)   
-
-        model.compile(loss=weighted_binary_crossentropy,
-                      optimizer='Adam',
-                      metrics=['accuracy'])
-        model.fit(X_train_2,
-                  Y_train_2,
-                  epochs=20,
-                  batch_size=2000,
-                  validation_data=(X_test_2, Y_test_2),
-                  verbose=verbose)
-
-        weights_push = reweight(theta0_G,model)
-        weights[i, 1:2, :] = weights_push
-        pass
-
-    return weights
+if __name__ == '__main__':
+    main(sys.argv[1:])
